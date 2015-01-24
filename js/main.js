f027b1be41e6dddf478ddcf4afe191ff3a835c60
senad87 <senadstore@gmail.com>
2015-01-24 16:31:29 +0100

    removed commented-out code

--- js/main.js
+++ js/main.js
@@ -1,41 +1,25 @@
 (function () {
 
-    //focused = undefined
+    var updateResult = function (nekiParam) {
+        return function() {
+            var sideA = $('.js-side-a').val();
+            var sideB = $('.js-side-b').val();
 
-    //$('.js-side-a').click(function () {
-    //    focused = $(this);
-    //});
-    //$('.js-side-b').click(function () {
-    //    focused = $(this);
-    //});
+            if (sideA !== '' && sideB !== '') {
 
-    var updateResult = function () {
-        var sideA = $('.js-side-a').val();
-        var sideB = $('.js-side-b').val();
+                var sideA = parseFloat(sideA, 10);
+                var sideB = parseFloat(sideB, 10);
 
-        if (sideA !== '' && sideB !== '') {
+                var obim = sideA * 2 + sideB * 2;
+                var povrshina = sideA * sideB;
 
-            var sideA = parseFloat(sideA, 10);
-            var sideB = parseFloat(sideB, 10);
-
-            var obim = sideA * 2 + sideB * 2;
-            var povrshina = sideA * sideB;
-
-            $('.content').css("margin-top", "0px");
-            $('.result h1').html("Obim: " + obim.toFixed(2) + " Povrshina: " + povrshina.toFixed(2));
+                $('.content').css("margin-top", "0px");
+                $('.result h1').html("Obim: " + obim.toFixed(2) + " Povrshina: " + povrshina.toFixed(2));
+            }
         }
     };
 
-    //$('.js-comma').click(function () {
-    //    if (typeof focused !== "undefined" && (focused.attr('class') === 'side js-side-a' || focused.attr('class') === 'side js-side-b')) {
-    //        var val = focused.val()
-    //        focused.val(val + ".1");
-    //        focused.focus();
-    //    }
-    //});
-
     $('.js-side-a').on('keyup', updateResult);
-
     $('.js-side-b').on('keyup', updateResult);
 
 })();
