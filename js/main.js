860759628b71f2d96d05af77f9c6a9bd9e9907a4
senad87 <senadstore@gmail.com>
2015-01-24 16:40:42 +0100

    fix

--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
 (function () {
 
-    var updateResult = function (nekiParam) {
-        return function() {
-            var sideA = $('.js-side-a').val();
-            var sideB = $('.js-side-b').val();
+    var updateResult = function () {
+        var sideA = $('.js-side-a').val();
+        var sideB = $('.js-side-b').val();
 
-            if (sideA !== '' && sideB !== '') {
+        if (sideA !== '' && sideB !== '') {
 
-                var sideA = parseFloat(sideA, 10);
-                var sideB = parseFloat(sideB, 10);
+            var sideA = parseFloat(sideA, 10);
+            var sideB = parseFloat(sideB, 10);
 
-                var obim = sideA * 2 + sideB * 2;
-                var povrshina = sideA * sideB;
+            var obim = sideA * 2 + sideB * 2;
+            var povrshina = sideA * sideB;
 
-                $('.content').css("margin-top", "0px");
-                $('.result h1').html("Obim: " + obim.toFixed(2) + " Povrshina: " + povrshina.toFixed(2));
-            }
+            $('.content').css("margin-top", "0px");
+            $('.result h1').html("Obim: " + obim.toFixed(2) + " Povrshina: " + povrshina.toFixed(2));
         }
     };
 
